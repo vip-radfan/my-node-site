ee75bb5a09a306267854a9b18600b99a8d2ec996
vip-radfan <vip.radfan@gmail.com>
2026-04-15 02:13:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-
 app.get("/", (req, res) => {
-    res.send("🔥 السيرفر شغال 24/7");
-});
-
+res.send("🔥 السيرفر شغال 24/7");
+}); 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-    console.log("Running...");
-});
+console.log("Running...");
+});     
